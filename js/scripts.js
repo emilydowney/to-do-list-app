26ad3611b694ce017fe02361eaa1e215c8567ed9
emilydowney <80785791+emilydowney@users.noreply.github.com>
2021-04-28 21:46:16 -0500

    Organize jQuery functions

--- js/scripts.js
+++ js/scripts.js
@@ -1,30 +1,33 @@
 function newItem(){
 
 //1. Adding a new item to the list of items:
-   let list = $('#list')
    let inputValue = $('#input').val();
-   let li = '<li>' + inputValue + '</li>'
+   let li = $('<li></li>');
+   li.append(inputValue);
 
    if (inputValue === '') {
      alert('You must write something!');
    } else {
-     list.append(li);
+     $('#list').append(li);
    }
 
  //2. Crossing out an item from the list of items:
-   $('li').on('click', function() {
-     $('li').addClass('strike');
-   });
+   function crossOut() {
+     li.addClass('strike');
+   };
 
- //3(i). Adding the delete button "X":
-   let crossOutButton = $('<button>X</button>');
-   $('li').append(crossOutButton);
+   li.on('dblclick', crossOut);
 
-   crossOutButton.on('click', deleteListItem());
- //3(ii). Adding CLASS DELETE (DISPLAY: NONE) from the css:
-   function deleteListItem(){
-     li.classList.add("delete")
-   }
+ //3(i). Adding the delete button "X" and delete function
+   let crossOutButton = $('<crossOutButton>X</crossOutButton>');
+   li.append(crossOutButton);
+
+   function deleteItem() {
+     li.remove();
+   };
+
+   crossOutButton.on('click', deleteItem);
+
  // 4. Reordering the items:
-   $('#list').sortable();
-}
+    $('#list').sortable();
+};
